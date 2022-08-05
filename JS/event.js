9932269224109bb98c2370f3d6357b6588c63a63
Jack0Bong <bonghj-wm21@student.tarc.edu.my>
2022-08-05 17:01:12 +0800

    added event page (WIP)
    created event page, imported Calender JS

---
+++ JS/event.js
@@ -0,0 +1,22 @@
+let calB = new Calendar({
+    id: "#calendar-b",
+    theme: "basic",
+    primaryColor: "#1a237e",
+    weekdayType: "short",
+    border: "5px solid rgba(4, 64, 160, 0.1)",
+    eventsData: [
+      {
+        id: 1,
+        name: "French class",
+        start: "2020-12-07T06:00:00",
+        end: "2020-12-09T20:30:00"
+      },
+      {
+        id: 2,
+        name: "Blockchain 101",
+        start: "2020-12-20T10:00:00",
+        end: "2020-12-20T11:30:00"
+      }
+    ]
+  });
+  
---
+++ event.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Main CSS import -->
+    <link rel="stylesheet" href="CSS/main.css">
+
+    <!-- Color Calendar Import -->
+    <script src="https://cdn.jsdelivr.net/npm/color-calendar@1.0.5/dist/bundle.js"></script>
+
+    <!-- Fonts CDN -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+
+    <!-- Color Calendar CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/color-calendar@1.0.5/dist/css/theme-basic.css" />
+</head>
+
+<body>
+    <!-- Font Awesome CDN -->
+    <script src="https://kit.fontawesome.com/deebd6333b.js" crossorigin="anonymous"></script>
+    <!-- Navigation bar import -->
+    <?php require("PHP_modules/Navigation.php") ?>
+
+    <div id="calendar-b"></div>
+
+    <!-- event JS import -->
+    <script src="JS/event.js"></script>
+    <!-- Footer import -->
+    <?php require("PHP_modules/footer.php") ?>
+    <!-- Bootstrap JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
