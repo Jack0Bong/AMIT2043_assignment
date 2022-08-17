33ad8706ce63ada7a2d496e28650a7a44c313825
tangwilson17 <tangw-wm21@student.tarc.edu.my>
2022-08-17 15:52:51 +0800

    about us fix

--- about_us.php
+++ about_us.php
@@ -125,48 +125,48 @@
     <!-- About us Profiles -->
     <div class="container mt-4 mb-4 p-3 d-flex ml-auto justify-content-between ">
         <div class="card p-4">
-            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager1.jpg" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
+            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager1.jpg" style="border-radius:50px;" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
                 <div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> </div>
                 <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
                 <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div>
-                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
+                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day. </span> </div>
                 <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
-                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div>
+                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May , 2021</span> </div>
             </div>
 
         </div>
         <div class="card p-4">
-            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager2.jpg" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
+            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager2.jpg" style="border-radius:50px;" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
                 <div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> </div>
                 <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
                 <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div>
-                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
+                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day. </span> </div>
                 <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
-                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div>
+                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May , 2021</span> </div>
             </div>
         </div>
     </div>
 
     <div class="container mt-4 mb-4 p-3 d-flex ml-auto justify-content-between ">
         <div class="card p-4">
-            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager3.jpg" height="100" width="100" style="border:" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
+            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light btn"> <img src="assets/manager3.jpg" style="border-radius:50px;" height="100" width="100" style="border:" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
                 <div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> </div>
                 <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
                 <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div>
-                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
+                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day. </span> </div>
                 <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
-                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div>
+                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May , 2021</span> </div>
             </div>
 
         </div>
         <div class="card p-4">
-            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager4.jpg" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
+            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-light"> <img src="assets/manager4.jpg " style="border-radius:50px;" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
                 <div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> </div>
                 <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
                 <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div>
-                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
+                <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br><br> Artist/ Creative Director by Day. </span> </div>
                 <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
-                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div>
+                <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May , 2021</span> </div>
             </div>
         </div>
     </div>
